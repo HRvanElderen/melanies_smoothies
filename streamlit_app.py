f86896660c99686621aa529be6c8117ce51eb15a
HRvanElderen <79331878+HRvanElderen@users.noreply.github.com>
2025-03-12 12:35:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,8 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 
-search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-st.write('The search value for ', fruit_chosen,' is ', search_on, '.')                                                                      
+
 
 name_on_order = st.text_input('', '', 25, placeholder='Name')
 
@@ -39,7 +37,9 @@ if ingredients_list:
     ingredients_str = ""
     for fruit in ingredients_list:
         st.subheader(fruit + ' Nutrition info')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')                                                                     
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
         ingredients_str += fruit + ' '
